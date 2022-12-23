c11c68bff4aee1d9c437cf4ff851564c1faa103a
lucianosants <lucianola18@gmail.com>
2022-12-23 20:44:03 -0300

    feat: initial config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import { GlobalStyles } from '../src/theme/GlobalStyles';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Creating some components</title>
+            </Head>
+            <GlobalStyles />
+            <Component {...pageProps} />)
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;

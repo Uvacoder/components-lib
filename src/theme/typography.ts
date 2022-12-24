df1145dc2e6ef6558c304bc95784b78e8415b25d
lucianosants <lucianola18@gmail.com>
2022-12-24 09:24:00 -0300

    style: converted px to rem

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,23 +1,23 @@
 const baseTypography = {
-    roboto_display_large: '57px/64px Roboto, sans-serif',
-    roboto_display__medium: '45px/52px Roboto, sans-serif',
-    roboto_display_small: '36px/44px Roboto, sans-serif',
+    roboto_display_large: '5.7rem/6.4rem Roboto, sans-serif',
+    roboto_display__medium: '4.5rem/5.2rem Roboto, sans-serif',
+    roboto_display_small: '3.6rem/4.4rem Roboto, sans-serif',
 
-    roboto_headline_large: '32px/40px Roboto, sans-serif',
-    roboto_headline_medium: '28px/36px Roboto, sans-serif',
-    roboto_headline_small: '24px/32px Roboto, sans-serif',
+    roboto_headline_large: '3.2rem/4.0rem Roboto, sans-serif',
+    roboto_headline_medium: '2.8rem/3.6rem Roboto, sans-serif',
+    roboto_headline_small: '2.4rem/3.2rem Roboto, sans-serif',
 
-    roboto_title_large: '22px/28px Roboto, sans-serif',
-    roboto_title_medium: '16px/24px Roboto, sans-serif',
-    roboto_title_small: '14px/20px Roboto, sans-serif',
+    roboto_title_large: '2.2rem/2.8rem Roboto, sans-serif',
+    roboto_title_medium: '1.6rem/2.4rem Roboto, sans-serif',
+    roboto_title_small: '1.4rem/2.0rem Roboto, sans-serif',
 
-    roboto_label_large: '14px/20px Roboto, sans-serif',
-    roboto_label_medium: '12px/16px Roboto, sans-serif',
-    roboto_label_small: '11px/16px Roboto, sans-serif',
+    roboto_label_large: '1.4rem/2.0rem Roboto, sans-serif',
+    roboto_label_medium: '1.2rem/1.6rem Roboto, sans-serif',
+    roboto_label_small: '1.1rem/1.6rem Roboto, sans-serif',
 
-    roboto_body_large: '16px/24px Roboto, sans-serif',
-    roboto_body_medium: '14px/20px Roboto, sans-serif',
-    roboto_body_small: '12px/16px Roboto, sans-serif',
+    roboto_body_large: '1.6rem/2.4rem Roboto, sans-serif',
+    roboto_body_medium: '1.4rem/2.0rem Roboto, sans-serif',
+    roboto_body_small: '1.2rem/1.6rem Roboto, sans-serif',
 };
 
 export { baseTypography };

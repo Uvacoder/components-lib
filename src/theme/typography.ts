d914c6c81ca0e3ddf3ce4535f252c4380a8d0146
lucianosants <lucianola18@gmail.com>
2022-12-24 08:33:18 -0300

    feat: themeing - add typography object

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,5 +1,23 @@
 const baseTypography = {
-    fonts: {},
+    roboto_display_large: '57px/64px Roboto, sans-serif',
+    roboto_display__medium: '45px/52px Roboto, sans-serif',
+    roboto_display_small: '36px/44px Roboto, sans-serif',
+
+    roboto_headline_large: '32px/40px Roboto, sans-serif',
+    roboto_headline_medium: '28px/36px Roboto, sans-serif',
+    roboto_headline_small: '24px/32px Roboto, sans-serif',
+
+    roboto_title_large: '22px/28px Roboto, sans-serif',
+    roboto_title_medium: '16px/24px Roboto, sans-serif',
+    roboto_title_small: '14px/20px Roboto, sans-serif',
+
+    roboto_label_large: '14px/20px Roboto, sans-serif',
+    roboto_label_medium: '12px/16px Roboto, sans-serif',
+    roboto_label_small: '11px/16px Roboto, sans-serif',
+
+    roboto_body_large: '16px/24px Roboto, sans-serif',
+    roboto_body_medium: '14px/20px Roboto, sans-serif',
+    roboto_body_small: '12px/16px Roboto, sans-serif',
 };
 
 export { baseTypography };

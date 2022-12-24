7d6f6fff8f4b3ea19f57d1bc4c47be50f0f93df8
lucianosants <lucianola18@gmail.com>
2022-12-24 09:22:53 -0300

    feat: themeing - add sizes object

---
+++ src/theme/sizes.ts
@@ -0,0 +1,10 @@
+const baseSizes = {
+    sizing_1: '0.8rem', // 8px
+    sizing_2: '1.2rem', // 12px
+    sizing_3: '1.6rem', // 16px
+    sizing_4: '2.4rem', // 24px
+    sizing_5: '3.2rem', // 32px
+    sizing_6: '4.8rem', // 48px
+};
+
+export { baseSizes };

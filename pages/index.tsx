a589aa638d0e0d6b86648cdaef50b82fc14e015b
lucianosants <lucianola18@gmail.com>
2022-12-28 18:13:34 -0300

    style: render Image component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 
 import Box from '@src/components/Box';
 import Text from '@src/components/Text';
+import Image from '@src/components/Image';
 
 const Home: NextPage = () => {
     return (
@@ -11,7 +12,8 @@ const Home: NextPage = () => {
                 flex
                 items="center"
                 justify="space-between"
-                h="300px"
+                wrap
+                // h="300px"
                 space="100px"
                 rounded="7px"
                 bg="primary"
@@ -19,19 +21,33 @@ const Home: NextPage = () => {
                 w="90%"
                 py="12px"
                 px="12px"
-                my="12px"
+                my="10rem"
                 mx="auto">
-                <Text as="h2" colors="errorVariant_2" inline italic weight="bold" underline>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur suscipit libero excepturi hic
-                    nesciunt labore voluptates doloremque placeat ipsa, temporibus quidem deserunt expedita nemo
-                    officiis, mollitia aut. Et, nam quidem.
-                </Text>
+                <Box as="article" flex space="2.8rem">
+                    <Text as="h2" colors="errorVariant_2" italic weight="bold" underline>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur suscipit libero excepturi
+                        hic nesciunt labore voluptates doloremque placeat ipsa, temporibus quidem deserunt expedita nemo
+                        officiis, mollitia aut. Et, nam quidem.
+                    </Text>
 
-                <Text as="span" align="end" inline size="lg">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus odio reprehenderit delectus
-                    assumenda. Odio aliquam omnis, hic doloremque, veritatis tenetur vitae, quas quia laboriosam
-                    consectetur quo ea odit repellendus impedit!
-                </Text>
+                    <Text as="span" align="end" size="lg">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus odio reprehenderit delectus
+                        assumenda. Odio aliquam omnis, hic doloremque, veritatis tenetur vitae, quas quia laboriosam
+                        consectetur quo ea odit repellendus impedit!
+                    </Text>
+                </Box>
+
+                <Box center>
+                    <Image
+                        src="https://github.com/lucianosants.png"
+                        alt="Image test"
+                        circle
+                        naturalWidth="80px"
+                        naturalHeight="100%"
+                        border
+                        borderColor="primaryVariant"
+                    />
+                </Box>
             </Box>
         </>
     );
